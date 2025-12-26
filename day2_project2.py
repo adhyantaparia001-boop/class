6e3d5eaf1541b3cf2094c539b6640a40dd559731
Acer <devanshtaparia011@gmail.com>
2025-12-26 19:22:44 +0545

    hello

---
+++ day2_project2.py
@@ -0,0 +1,3 @@
+firstname = input("Enter your first name : ")
+lastname = input("Enter your last name : ")
+print(f"{firstname} {lastname} the sum of the length of the string is {len(firstname)+len(lastname)} ")
